f0bc874cdfa5d492e72071420c34986cf291310a
Amry <amry@users.noreply.github.com>
2016-10-13 20:52:03 +0200

    add working solution for module 1

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,35 +9,36 @@
 
   function LunchCheckController($scope) {
     $scope.items = '';
-    $scope.errorMessage = '';
+    $scope.errorMassage = '';
 
     $scope.checkIt = function(items) {
       if (!items ) {
-        $scope.errorMessage = "Please enter data first!"
+          $scope.errorMassage = "Please enter data first!";
+          $scope.massageClass = "alert alert-warning";
       } else {
-        var lunchItems = cleanUp(items);
-        if (lunchItems.length>0) {
-          if (lunchItems.length <= 3) {
-            $scope.errorMessage = "Enjoy!"
-          } else {
-              $scope.errorMessage = "Too much!";
+          var lunchItems = cleanUp(items);
+          if (lunchItems.length>0) {
+              if (lunchItems.length <= 3) {
+                  $scope.massageClass = "alert alert-success";
+                  $scope.errorMassage = "Enjoy!";
+                } else {
+                  $scope.errorMassage = "Too much!";
+                  $scope.massageClass = "alert alert-success";
+                }
+          }else {
+                $scope.errorMassage = "Your input is empty";
+                $scope.massageClass = "alert alert-success";
           }
-        }else {
-          $scope.errorMessage = "Your input is empty";
-        }
-
       }
-
     }
 
     function cleanUp(item) {
       var itemArray = item.split(",");
       var arrayToReturn = [];
-      itemArray.forEach(function(currentItem, index, array) {
-        if (currentItem || !currentItem ==="")
+      itemArray.forEach(function(currentItem) {
+        if (currentItem.trim().length > 0)
           arrayToReturn.push(currentItem);
       });
-      console.log(arrayToReturn);
       return arrayToReturn;
     }
   }
