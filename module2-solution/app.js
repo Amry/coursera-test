ce210468ed290c8d9f0de71e15faaef15e395deb
Amry <amry@users.noreply.github.com>
2016-10-17 23:04:15 +0200

    move glyphicons into cparent folder, add first version of solution for module 2

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -8,18 +8,47 @@
 
   ToBuyController.$inject = ['ShoppingListCheckOffService'];
 
-  function ToBuyController() {
-
+  function ToBuyController(ShoppingListCheckOffService) {
+      this.Items = ShoppingListCheckOffService.getBuyItems();
+      this.removeItem = function (itemIndex) {
+          ShoppingListCheckOffService.removeItem(itemIndex);
+      };
   }
 
   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 
-  function AlreadyBoughtController() {
-
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+      this.Items = ShoppingListCheckOffService.getBoughtItems();
+      console.log("Bought Items: " + this.Items);
   }
 
   function ShoppingListCheckOffService() {
+    var boughtList = [];
+    var buyList = [
+            { name: "non (bread)", quantity: 5 },
+            { name: "shir (milk)", quantity: 2 },
+            { name: "tukhm (egg)", quantity: 10 },
+            { name: "gusht (meat)", quantity: 2 },
+            { name: "qand (sweets)", quantity: 20 }
+    ];
+
+    this.removeItem = function(itemIndex) {
+      addItem(buyList[itemIndex]);
+      buyList.splice(itemIndex, 1);
+    };
+
+    var addItem = function(items) {
+      boughtList.push(items);
+    }
+
+    this.getBuyItems = function() {
+      return buyList;
+    };
+
+    this.getBoughtItems = function() {
+      return boughtList;
+    }
 
   }
-  
+
 }) ();
